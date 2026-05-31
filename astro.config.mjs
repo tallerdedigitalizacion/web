abf6995a76b73248334be8d2f49bf2d01d349f1a
Pablo Leone <info@tallerdedigitalizacion.com>
2026-05-31 02:29:06 +0200

    Deploy static site to GitHub Pages

--- astro.config.mjs
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "astro/config";
-
-const site = process.env.PUBLIC_SITE_URL || "https://tallerdedigitalizacion.com";
-
-export default defineConfig({
-  site,
-  output: "static",
-});
