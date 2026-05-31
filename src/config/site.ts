e4247f9e24a4b27b23f3b3977c60dc527c6a0012
Pablo Leone <info@tallerdedigitalizacion.com>
2026-05-31 19:26:11 +0200

    wip

--- src/config/site.ts
+++ src/config/site.ts
@@ -2,27 +2,18 @@ export const siteConfig = {
   name: "Taller de Digitalización",
   seoTitle: "Diagnóstico de Caos Operativo para PyMEs | Taller de Digitalización",
   seoDescription:
-    "Ayudo a CEOs de PyMEs a detectar y reducir el caos operativo: procesos invisibles, dependencia del dueño, herramientas mal alineadas y fricción que impide avanzar.",
+    "Diagnóstico de Caos Operativo es una auditoría estructurada para pymes que sienten que la empresa funciona, pero con demasiada dependencia del CEO y fricción interna constante. Analizamos procesos, comunicación, herramientas y flujo de trabajo para identificar cuellos de botella, dependencias críticas y desorden operat.",
   publicUrl: "https://tallerdedigitalizacion.com",
   bookingUrl: "https://calendly.com/tallerdedigitalizacion-info/30min",
   linktreeUrl: "https://linktr.ee/tallerdedigitalizacion",
   whatsappNumber: "34684768974",
   contactEmail: "info@tallerdedigitalizacion.com",
-  googleAnalyticsId: "[INSERTAR_GA4_ID]",
+  googleAnalyticsId: "G-4GY58K423T",
   chatbaseId: "V8ryUqCVZxb4N8Hnym06s",
   ogImage: "/og-image.jpg",
   logo: "/logo.png",
-  formEndpoint: "[INSERTAR_ENDPOINT_FORMULARIO]",
+  formEndpoint: "[INSERTAR_ENDPOINT_FORMULARIO]", // TODO
   methodPdfPath: "/downloads/metodo-auditoria-caos-operativo.pdf",
-  legal: {
-    owner: "[INSERTAR_TITULAR]",
-    taxId: "[INSERTAR_NIF_CIF]",
-    address: "[INSERTAR_DOMICILIO_FISCAL]",
-    dataController: "[INSERTAR_RESPONSABLE_DEL_TRATAMIENTO]",
-    hostingProvider: "[INSERTAR_PROVEEDOR_DE_HOSTING]",
-    thirdPartyTools:
-      "[INSERTAR_HERRAMIENTAS_DE_TERCEROS: Google Analytics, Chatbase, WhatsApp, Calendly, Linktree, Formspree/Netlify/Google Forms]",
-  },
 };
 
 export const whatsappUrl = `https://wa.me/${siteConfig.whatsappNumber}?text=Hola,%20vengo%20de%20la%20web%20de%20Taller%20de%20Digitalizaci%C3%B3n.%20Quer%C3%ADa%20consultar%20por%20el%20diagn%C3%B3stico%20operativo.`;
